332a3659441cc97a8d78e2cba843c2070b4d79e1
majidgolbaz <majidgolbaz@gmail.com>
2023-04-17 19:35:29 +0430

    [WIP-MGO] ROUTES

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.scss";
 import ProductCard from "./components/ProductCard";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <ProductCard />
-    </div>
-  );
+  return <R  outes>
+    <Route>
+      
+    </Route>
+  </R>
 }
 
 export default App;
