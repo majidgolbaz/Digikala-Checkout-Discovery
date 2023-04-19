a4a500cf84134ec7aa8641855070bc72fbc53f98
majidgolbaz <majidgolbaz@gmail.com>
2023-04-19 10:11:59 +0430

    [WIP-MGO] add route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./App.scss";
 import ProductCard from "./components/ProductCard";
 import { Route, Routes } from "react-router-dom";
+import ProductListPage from "./pages/ProductList";
+import ModalPlayground from "./pages/Playground/modal";
+import ProductCardPage from "./pages/Playground/productCard";
+import CartPage from "./pages/Cart";
 
 function App() {
-  return <R  outes>
-    <Route>
-      
-    </Route>
-  </R>
+  return (
+    
+    <Routes>
+      <Route path="/" element={<ProductListPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/playground/modal" element={<ModalPlayground />} />
+      <Route path="/playground/productCard" element={<ProductCardPage />} />
+    </Routes>
+  );
 }
 
 export default App;
