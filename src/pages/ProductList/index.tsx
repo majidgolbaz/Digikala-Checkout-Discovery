842d5f4b866e0665935af14682c02571dd1c51f8
majidgolbaz <majidgolbaz@gmail.com>
2023-04-24 10:01:13 +0430

    [ADD-MGO] add routes

--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface IProductListPageProps {
   customClass?: string;
 }
+
 function ProductListPage({ customClass = "" }: IProductListPageProps) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products`)
+      .then((res) => res.json())
+      .then((json) => console.log(json.products));
+  }, []);
+
   return <div className={customClass}>ProductListPage</div>;
 }
 
